e47908128f671d9d49de6bf7841ca8706375db6d
Bob Martin <bob@example.com>
2013-03-06 12:13:59 +0100

    Début de VieCell

---
+++ src/main/java/IUT/BoBot/SmartCells/VieCell.java
@@ -0,0 +1,19 @@
+package IUT.BoBot.SmartCells;
+
+import IUT.BoBot.SmartCell;
+
+public class VieCell implements SmartCell {
+	
+	public String ask(String dateNaiss) {
+		if(dateNaiss.contains("né"))
+		  
+		else
+		  return null;
+	}
+
+}
+
+/*Je suis né le 8 février 1985.
+# => Vous êtes né un Vendredi, aujourd'hui est votre 10310 jour de
+vie. Il vous reste environ 20774 jours à vivre.
+*/
